573d35945bd20e3aa204a3bc21b53516fc7e763b
Ezequiel040 <93410576+Ezequiel040@users.noreply.github.com>
2024-04-04 17:44:24 -0700

    Update loginP.js

--- static/loginP.js
+++ static/loginP.js
@@ -1,13 +1,21 @@
 function verifyIdentity(){
 
 };
+function createAccount() {
+    const usrnm = document.getElementById("username").value;
+    const passwrd = document.getElementById("password").value;
+    const isTeacher = document.getElementById("is_teacher").checked;
 
-function createAccount(){
+    const data = { "username": usrnm, "password": passwrd, "is_teacher": isTeacher };
 
-};
-
-// document.addEventListener("DOMContentLoaded", function(){
-//     document.getElementById("createAcctB").onclick = function() {
-//         window.location.href = "createAcc.html"
-//     };
-// });
+    fetch('http://127.0.0.1:5000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => console.log('Success:', data))
+    .catch(error => console.error('Error:', error));
+}
